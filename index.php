1d7e81567bf315f60990818260a50fea0f1cff8b
Wello_Maqar <waleed.maqar.dev89@gmail.com>
2024-02-19 18:33:15 +0200

    add products container
    Add div contains all products and make at a grid with styling for single product div

--- index.php
+++ index.php
@@ -10,7 +10,19 @@
 
 <body>
   <H1 id="siteTitle">GIT training</H1>
-
+  <div class="product-container">
+    <?php $count = 7;
+    for ($i = 1; $i <= $count; $i++) :
+    ?>
+    <div class="product-card">
+      <h1>Test product <?= $i ?></h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus nulla magnam ipsa amet modi minima distinctio
+        sint quidem, labore reprehenderit omnis tempore blanditiis cumque deleniti consequatur tenetur quo a. Saepe.
+      </p>
+    </div>
+    <?php endfor; ?>
+  </div>
   <script src="main.js"></script>
 </body>
 
